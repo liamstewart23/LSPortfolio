0e8a276f0c0946bb98348e8b844f23e02dd3c603
Liam Stewart <lstewart2397@gmail.com>
2017-06-19 16:51:48 -0400

    fixing aria-hidden tag in php echo

--- includes/contact.php
+++ includes/contact.php
@@ -22,7 +22,7 @@
 						if(!is_string($getContact)){
 						while($row = mysqli_fetch_array($getContact)){
 						echo "<div class=\"col-sm-12 col-md-4\">";
-							echo "<i class=\"fa fa-{$row['c_title']}\" aria-hidden="true"></i></i>  <a href=\"{$row['c_link']}\" target=\"_blank\">{$row['c_handle']}</a><br><br>";
+							echo "<i class=\"fa fa-{$row['c_title']}\" aria-hidden=\"true\"></i></i>  <a href=\"{$row['c_link']}\" target=\"_blank\">{$row['c_handle']}</a><br><br>";
 						echo "</div>";
 						}
 						}
