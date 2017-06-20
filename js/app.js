abf792642ac0a9ebf59c9c9754dc87f3628d6eb2
Liam Stewart <lstewart2397@gmail.com>
2017-06-20 17:22:43 -0400

    Adding project type filter

--- js/app.js
+++ js/app.js
@@ -143,6 +143,23 @@ app.controller('PortfolioCtrl', [function () {
             opacity: 1,
             x: 0
         });
+        //Portfolio Item Type
+        $(".filter-button").click(function () {
+            var value = $(this).attr('data-filter');
+            if (value == "all") {
+                $('.filter').show('1000');
+            }
+            else {
+                $(".filter").not('.' + value).hide('3000');
+                $('.filter').filter('.' + value).show('3000');
+
+            }
+        });
+
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+        $(this).addClass("active");
     });
 }]);
 //Controller for Portfolio Item
