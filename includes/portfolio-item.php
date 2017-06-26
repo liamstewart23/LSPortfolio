63cd8c5953b4437848fd62e2e66fa7baf40968d4
Liam Stewart <lstewart2397@gmail.com>
2017-06-26 14:29:57 -0400

    adding link to source and website on item page

--- includes/portfolio-item.php
+++ includes/portfolio-item.php
@@ -4,22 +4,22 @@
     <div class="col-xs-12 col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1">
       <div ng-model="works">
         <div class="col-xs-12 col-md-12">
-        <a href="#/portfolio" class="pull-right"><i class="fa fa-arrow-circle-left"></i> Back to Portfolio</a>
+        <a href="#/portfolio" class="pull-right"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i> Back to Portfolio</a>
         </div>
         <h2>{{works[whichWork].title}}</h2>
         <!--<img ng-src="images/{{works[whichWork].shortname}}.jpg" alt="Photo of {{works[whichWork].title}}">-->
-        <p ng-show="works[whichWork].source">{{'Source: ' + works[whichWork].source}}</p>
-        <p ng-show="works[whichWork].project">{{'View Project: ' + works[whichWork].project}}</p>
+        <p ng-show="works[whichWork].project"><a href="{{works[whichWork].project}}" target="_blank"><i class="fa fa-desktop" aria-hidden="true"></i> View Project</a></p>
+        <p ng-show="works[whichWork].source"><a href="{{works[whichWork].source}}" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> View Source</a></p>
         <p>{{'Media: ' + works[whichWork].media}}</p>
         <p>{{'Media: ' + works[whichWork].tech}}</p>
         <!--<p>{{works[whichWork].description}}</p>-->
       </div>
       <div class="row portfolioItemFooterNav">
         <div class="col-xs-6 col-sm-6 col-md-3">
-          <a href="#/portfolio-item/{{prevItem}}"><i class="fa fa-arrow-left"></i> {{works[prevItem].title}}</a>
+          <a href="#/portfolio-item/{{prevItem}}"><i class="fa fa-arrow-left" aria-hidden="true"></i> {{works[prevItem].title}}</a>
         </div>
           <div class="col-xs-6 col-sm-6 col-md-3 col-md-offset-6">
-          <a class="pull-right" href="#/portfolio-item/{{nextItem}}">{{works[nextItem].title}} <i class="fa fa-arrow-right"></i></a>
+          <a class="pull-right" href="#/portfolio-item/{{nextItem}}">{{works[nextItem].title}} <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
