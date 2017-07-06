b17fc093781553dd3b50d0dbedcccc2aa52f6e50
Liam Stewart <lstewart2397@gmail.com>
2017-07-06 13:05:00 -0400

    Pulling menu over on mobile

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,10 @@
 	<h2 class="hide">Website Navigation</h2>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-8 col-sm-offset-0 col-md-2">
+			<div class="col-xs-10 col-sm-offset-0 col-md-2">
 				<a class="navbar-brand" href="index.php?partial=home" id="logo">L</a>
 			</div>
-			<div class="col-xs-4 col-sm-offset-0 col-md-5 col-md-offset-5">
+			<div class="col-xs-2 col-sm-offset-0 col-md-5 col-md-offset-5 pull-right">
 				<ul class="nav navbar-nav navbar-right ham">
 					<li>
 						<a href="index.php?partial=menu-no-js" id="menu" class="menu menuAnimate">
